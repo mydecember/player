cbb46a1670d9fa016588defde3462bb1bd3ea172
zhaofuqiang <zhaofuqiang@xiaomi.com>
2020-03-31 22:44:48 +0800

    opensles

---
+++ app/src/main/cpp/audio_device/OpenSLESRecorder.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zfq on 2020/3/31.
+//
+
+#include "OpenSLESRecorder.h"
---
+++ app/src/main/cpp/audio_device/OpenSLESRecorder.h
@@ -0,0 +1,20 @@
+//
+// Created by zfq on 2020/3/31.
+//
+
+#ifndef TESTCODEC_OPENSLESRECORDER_H
+#define TESTCODEC_OPENSLESRECORDER_H
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_Android.h>
+#include <SLES/OpenSLES_AndroidConfiguration.h>
+
+class OpenSLESRecorder {
+public:
+    OpenSLESRecorder();
+    ~OpenSLESRecorder();
+
+private:
+};
+
+
+#endif //TESTCODEC_OPENSLESRECORDER_H
